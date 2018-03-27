176c8f2f845b36c4931eb27291c9b8739f2f5a85
Eric Tomlin <Superservercraft@gmail.com>
2018-03-26 21:14:37 -0400

    All options structs are now parsed correctly

--- src/cfgMgr.go
+++ src/cfgMgr.go
@@ -4,21 +4,39 @@ import (
     "fmt"
     "os"
     "github.com/BurntSushi/toml"
+    "flag"
 )
 
 type Config struct {
-	themeTemplate  string
-  searchTemplate string
-  itemTemplate   string
-  tag_contents   string
-  tag_class      string
-  tag_file_href  string
-  tag_item_type  string
-  tag_root_step  string
-  tag_domain     string
-  tag_root_dir   string
-  tag_sidenav    string
-  tag_breadcrumb string
+	ThemeTemplate  string
+  SearchTemplate string
+  ItemTemplate   string
+  Include_path   string
+  Tag_contents   string
+  Tag_class      string
+  Tag_file_href  string
+  Tag_item_type  string
+  Tag_root_step  string
+  Tag_domain     string
+  Tag_root_dir   string
+  Tag_sidenav    string
+  Tag_breadcrumb string
+}
+
+type Arguments struct {
+  Verbose   *bool
+  Version   *bool
+  Quiet     *bool
+  SuperQuiet *bool
+  Force     *bool
+  Webroot   *string
+  Unjail    *bool
+  Filename  *string
+  Sort      *bool
+  OutFile   *string
+  ConfigFile *string
+  WorkPath  string
+  Tail      []string
 }
 
 // Reads info from config file
@@ -30,11 +48,44 @@ func ReadConfig(path string) Config {
     os.Exit(1)
 	}
 
-	var config Config
-	if _, err := toml.DecodeFile(path, &config); err != nil {
+	var conf Config
+	if _, err := toml.DecodeFile(path, &conf); err != nil {
 		fmt.Println(err.Error())
     os.Exit(1)
 	}
-	//log.Print(config.Index)
-	return config
+	return conf
+}
+
+func ReadArgs() Arguments {
+  args := Arguments{}
+
+  args.Verbose    = flag.Bool("v", false, "Verbose: Make the program give more detailed output.")
+  args.Version    = flag.Bool("V", false, "Version: Get program version and some extra info.")
+  args.Quiet      = flag.Bool("q",false, "Quiet: Decrease Logging Levels to Warning+")
+  args.SuperQuiet = flag.Bool("qq",false, "Super Quiet: Makes the program not output to stdout, only displaying fatal errors.")
+  args.Force      = flag.Bool("F", false, "Force: Force-regenerate all directories, even if no changes have been made.")
+  args.Webroot    = flag.String("w", "", "Webroot: Specify a webroot to jail symlinks to.")
+  args.Unjail     = flag.Bool("u", false, "Unjail: Use to remove the restriction jailing symlink destinations to the webroot.")
+  args.Filename   = flag.String("f", "index.html", "File: Manually set the name of the HTML file containing the directory listing.")
+  args.Sort       = flag.Bool("s", false, "Sort: Sort directory entries alphabetically.")
+  args.OutFile    = flag.String("o", "", "lOgfile: Path to a text file to write program output to (file will be overwritten!). Use along with -qq to output to file and not stdout.")
+  args.ConfigFile = flag.String("c", (os.Getenv("HOME") + "/.config/godir/config.toml"), "Specify a file to use as the godir config.")
+
+  flag.Parse()
+
+  tail := flag.Args()
+  if (len(tail) == 0) {
+    fmt.Println("No workpath supplied. Use the `-h` flag for help.")
+  } else {
+    args.WorkPath = tail[0]
+    index, err := indexOf(tail, args.WorkPath)
+    if (err != nil) {
+      fmt.Println("indexOf failed with error: " + err.Error())
+      os.Exit(1)
+    }
+    tail = remove(tail, index)
+  }
+  args.Tail = tail
+
+  return args
 }
--- src/main.go
+++ src/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "os"
-    "flag"
+    "time"
 )
 
 func main() {
+
+  // Time program execution
+  start := time.Now()
+
 
   Ver := "0.1.2"  // Version
   Rev := "a"      // Revision (how many times has this version been committed to fix bugs.)
@@ -16,70 +21,51 @@ func main() {
     fmt.Println("No arguments supplied. Use the `-h` flag for help.")
     os.Exit(0)
   }
-  // *** Argparse Stuff *** //
 
-  // var := flag.Type("flag", default, "Desc")
-  verbose := flag.Bool("v", false, "Verbose: Make the program give more detailed output.")
-  version := flag.Bool("V", false, "Version: Get program version and some extra info.")
-  quiet   := flag.Bool("q",false, "Quiet: Decrease Logging Levels to Warning+")
-  superQuiet := flag.Bool("qq",false, "Super Quiet: Makes the program not output to stdout, only displaying fatal errors.")
-  force   := flag.Bool("F", false, "Force: Force-regenerate all directories, even if no changes have been made.")
-  webroot := flag.String("w", "", "Webroot: Specify a webroot to jail symlinks to.")
-  unjail   := flag.Bool("u", false, "Unjail: Use to remove the restriction jailing symlink destinations to the webroot.")
-  filename   := flag.String("f", "index.html", "File: Manually set the name of the HTML file containing the directory listing.")
-  sort   := flag.Bool("s", false, "Sort: Sort directory entries alphabetically.")
-  outFile   := flag.String("o", "", "lOgfile: Path to a text file to write program output to (file will be overwritten!). Use along with -qq to output to file and not stdout.")
-  configFile := flag.String("c", (os.Getenv("HOME") + "/.config/godir/config.toml"), "Specify a file to use as the godir config.")
-
-  flag.Parse()
+  // Load the args.
+  args := ReadArgs()
+  config := ReadConfig(*args.ConfigFile)
 
   // --version output
-  if ( *version ) {
+  if ( *args.Version ) {
     fmt.Println("godir V." + Ver + Rev)
     fmt.Println("\ngodir is Licensed under the GNU GPL v3.\nCode copyright (c) 2018 Nicolas \"Montessquio\" Suarez.")
     os.Exit(0)
   }
 
-  // *** END Argparse Stuff *** //
-
-  // *** Logger Config *** //
-
   // func Logger(level int, sendILogs bool, quiet bool, oFile string) *_logger
-  console := Logger(2, *verbose, *quiet, *outFile)
-  // *** END Logger Config *** //
-
-  // *** Set up tail *** //
-
-  tail := flag.Args()
-  workPath := tail[0]
-  index, err := indexOf(tail, workPath)
-  if (err != nil) {
-    console.Error("indexOf failed with error: " + err.Error())
-  }
-  tail = remove(tail, index)
-
-  // ** End Set up tail *** //
+  console := Logger(2, *args.Verbose, *args.Quiet, *args.OutFile)
 
   // *** Debug Mode Sanity Output *** //
 
-  if ( *verbose ) { // AKA test mode.
+  if ( *args.Verbose ) { // AKA test mode.
     console.Ilog("The following Args were Parsed:")
-    fmt.Printf("Verbose %t\n", *verbose)
-    fmt.Printf("Version %t\n", *version)
-    fmt.Printf("Quiet %t\n", *quiet)
-    fmt.Printf("SQuiet %t\n", *superQuiet)
-    fmt.Printf("Force %t\n", *force)
-    fmt.Printf("Webroot %q\n", *webroot)
-    fmt.Printf("Unjail %t\n", *unjail)
-    fmt.Printf("Filename %q\n", *filename)
-    fmt.Printf("Sort %t\n", *sort)
-    fmt.Printf("oFile %q\n", *outFile)
-    fmt.Printf("cfgFile %q\n", *configFile)
-    fmt.Printf("workPath %q\n", workPath)
-    fmt.Printf("tail: %q\n", tail)
+    /*
+    fmt.Printf("Verbose %t\n", *args.Verbose)
+    fmt.Printf("Version %t\n", *args.Version)
+    fmt.Printf("Quiet %t\n", *args.Quiet)
+    fmt.Printf("SQuiet %t\n", *args.SuperQuiet)
+    fmt.Printf("Force %t\n", *args.Force)
+    fmt.Printf("Webroot %q\n", *args.Webroot)
+    fmt.Printf("Unjail %t\n", *args.Unjail)
+    fmt.Printf("Filename %q\n", *args.Filename)
+    fmt.Printf("Sort %t\n", *args.Sort)
+    fmt.Printf("oFile %q\n", *args.OutFile)
+    fmt.Printf("cfgFile %q\n", *args.ConfigFile)
+    fmt.Printf("workPath %q\n", args.WorkPath)
+    fmt.Printf("tail: %q\n", args.Tail)
+    */
+    data, err := json.Marshal(args)
+    if err != nil { console.Error(err.Error()) }
+    fmt.Printf("%s\n", data)
+
+    console.Ilog("\nThe following configuration options were loaded from " + *args.ConfigFile + ":")
+    data, err = json.Marshal(config)
+    if err != nil { console.Error(err.Error()) }
+    fmt.Printf("%s\n", data)
   }
 
-  // *** END Debug Mode Sanity Output *** //
 
-
+  // Program end.
+  fmt.Printf("Done. Took %s\n", time.Since(start))
 }
