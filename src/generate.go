fa6aa1220088322d864360be950b7ef1f028643c
Nicolas Suarez <nicolas.a.suarez19@gmail.com>
2018-06-26 15:54:02 -0400

    Fix escaping bug caused by previous fix

--- src/generate.go
+++ src/generate.go
@@ -143,7 +143,7 @@ func regen(path string, wg *sync.WaitGroup, semaphore chan struct{}) {
         tmp = SubTag(tmp, opts.Conf.Tag_filesize, FileSizeCount(DirSize(path + "/" + file.Name())))
         tmp = SubTag(tmp, opts.Conf.Tag_filename, file.Name())
         tmp = SubTag(tmp, opts.Conf.Tag_last_modified, file.ModTime().Format("2006-01-02 15:04:05"))
-        tmp = SubTag(tmp, opts.Conf.Tag_file_href, url.QueryEscape("./" + file.Name() + "/" + *opts.Args.Filename))
+        tmp = SubTag(tmp, opts.Conf.Tag_file_href, "./" + url.QueryEscape(file.Name()) + "/" + url.QueryEscape(*opts.Args.Filename))
 
         // Append the composed item to file.
         err = AppendFile(path + "/" + *opts.Args.Filename, []byte(tmp))
@@ -180,7 +180,7 @@ func regen(path string, wg *sync.WaitGroup, semaphore chan struct{}) {
       tmp = SubTag(tmp, opts.Conf.Tag_filesize, FileSizeCount(file.Size()))
       tmp = SubTag(tmp, opts.Conf.Tag_filename, file.Name())
       tmp = SubTag(tmp, opts.Conf.Tag_last_modified, file.ModTime().Format("2006-01-02 15:04:05"))
-      tmp = SubTag(tmp, opts.Conf.Tag_file_href, url.QueryEscape("./" + file.Name()))
+      tmp = SubTag(tmp, opts.Conf.Tag_file_href, "./" + url.QueryEscape(file.Name()))
 
       // Append the composed item to file.
       err = AppendFile(path + "/" + *opts.Args.Filename, []byte(tmp))
--- src/misc.go
+++ src/misc.go
@@ -311,7 +311,7 @@ func GenSidenav(path string, streak int) { // Streak needs to start at 0
 
 
 				// Set up some values real quick
-				fullpath := url.QueryEscape(path + "/" + p.Name())
+				fullpath := path + "/" + url.QueryEscape(p.Name())
 				// if is not empty
 				if !isEmpty {
 					sideNav += `<li class="pure-menu-item has-children"><div class="side-checkbox"><input type="checkbox" onclick="dropdown(this)" id="collapse_` + uid + `"/><label class="list-collapsed-icon" for="collapse_` + uid + `" id="chevron_` + uid + `"></label></div><div class="side-content" id="a1"><a href="$root-step$/` + fullpath + `" class="pure-menu-link">` + p.Name() + `</a></div>` + `<ul class="pure-menu-list"></ul>` + `<ul class="pure-menu-list default-hidden" id="` + uid + `">`
